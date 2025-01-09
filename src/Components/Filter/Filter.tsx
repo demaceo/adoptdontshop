d3d16708b7825a8e5aa1632a601c51b05862f295
demaceo <hdemaceo@gmail.com>
2025-01-08 19:19:54 -0700

    create Filter component

---
+++ src/Components/Filter/Filter.tsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+import "./Filter.css"
+
+export default function Filter() {
+    // TODO: Implement Filter component with checkboxes for different filters (type, breed, size, coat, gender, age, distance, etc.)
+  return (
+    <div>Filter</div>
+  )
+}
