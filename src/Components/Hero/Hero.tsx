0b0352602ab8c25b1fefc9aa981b2b3514137197
demaceo <hdemaceo@gmail.com>
2025-01-31 16:30:37 -0700

    construct hero

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import "./Hero.css"
-export default function Hero() {
+import "./Hero.css";
+
+const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero">
+      {/* <img src="/path-to-image.jpg" alt="Hero" className="hero-image" /> */}
+      <div className="hero-text">
+        <h1>Adopt a Pet, Change a Life</h1>
+        <p>Furry friends are waiting for you!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
