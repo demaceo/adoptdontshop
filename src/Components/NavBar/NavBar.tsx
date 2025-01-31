097bccdbd014bdd13a7c3433ad356bdb4c835a11
demaceo <hdemaceo@gmail.com>
2025-01-31 16:30:29 -0700

    build NavBar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
-import "./NavBar.css"
-export default function NavBar() {
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">Adopt Don't Shop</Link>
+      </div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/vibe-check">Vibe Check</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/login">Log-In / Sign Up</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
