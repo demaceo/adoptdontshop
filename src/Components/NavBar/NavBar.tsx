3221fa3a3eaa4ba4dedd4633dffb587866162b1a
demaceo <hdemaceo@gmail.com>
2025-02-06 19:54:59 -0700

    add Contact Link

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -9,8 +9,9 @@ const Navbar = () => {
       </div>
       <div className="nav-links">
         <Link to="/">Home</Link>
-        <Link to="/vibe-check">Vibe Check</Link>
+        {/* <Link to="/vibe-check">Vibe Check</Link> */}
         <Link to="/favorites">Favorites</Link>
+        <Link to="/contact">Contact</Link>
         <Link to="/login">Log-In / Sign Up</Link>
       </div>
     </nav>
