bb0524bbd51bc3cd9cbda5a15fa6622cfaf4e989
demaceo <hdemaceo@gmail.com>
2025-02-09 16:53:47 -0700

    adjust svg attributes for React

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -12,9 +12,9 @@ const DEFAULT_IMAGE = (
     version="1.1"
     id="Capa_1"
     xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
     viewBox="0 0 228.804 228.804"
-    xml:space="preserve"
+    xmlSpace="preserve"
   >
     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
     <g
