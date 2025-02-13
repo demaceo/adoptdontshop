95ef97aaf0509549cec5b5c82e37eeb2ea8a46ad
demaceo <hdemaceo@gmail.com>
2025-02-12 18:41:53 -0700

    tennis ball animation (temporary)

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -3,11 +3,14 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="hero">
-      {/* <img src="/path-to-image.jpg" alt="Hero" className="hero-image" /> */}
       <div className="hero-text">
         <h1>Adopt a Pet, Change a Life</h1>
         <p>Furry friends are waiting for you!</p>
       </div>
+      <div className="tennis-ball-container">
+        <div className="tennis-ball"></div>
+      </div>
+
     </div>
   );
 };
