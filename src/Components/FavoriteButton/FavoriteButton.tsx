3dc036177f94f1b9108b5167984501a97cf03e41
demaceo <hdemaceo@gmail.com>
2025-02-18 16:57:13 -0700

    remove unused code

--- src/Components/FavoriteButton/FavoriteButton.tsx
+++ src/Components/FavoriteButton/FavoriteButton.tsx
@@ -24,67 +24,50 @@ export default function FavoriteButton({ id, petData }: FavoriteButtonProps) {
     setIsFavoritedState(isFavorited(id));
   }, [id, isFavorited]);
 
-  // const animateAddFavorite = () => {
-  //   const tl = gsap.timeline();
-  //   gsap.set(`.${"--" + id}`, {
-  //     transition: "ease 0",
-  //     transform: "rotate(0deg)",
-  //   });
+  const animateAddFavorite = () => {
+    const tl = gsap.timeline();
+    gsap.set(`.${"--" + id}`, {
+      //start animation state
+      transition: "ease 0",
+      transform: "rotate(0deg)",
+    });
 
-  //   tl.to(`.${"--" + id}`, { duration: 0.1, translateY: 3 })
-  //     .to(`.${"--" + id}`, { duration: 0.3, rotateY: 360, translateY: -10 })
-  //     .to(`.${"--" + id}`, { duration: 0.3, translateY: 0 })
-  //     .to(
-  //       `.${"--" + id}`,
-  //       { duration: 0.2, filter: "grayscale(0%)", cursor: "default" },
-  //       "-=.4"
-  //     );
-  // };
-
-    const animateAddFavorite = () => {
-      const tl = gsap.timeline();
-      gsap.set(`.${"--" + id}`, {
-        //start animation state
-        transition: "ease 0",
-        transform: "rotate(0deg)",
+    tl.to(`.${"--" + id}`, { duration: 0.1, translateY: 3 })
+      .to(`.${"--" + id}`, { duration: 0.3, rotateY: 360, translateY: -10 })
+      .to(`.${"--" + id}`, { duration: 0.3, translateY: 0 })
+      .to(
+        `.${"--" + id}`,
+        { duration: 0.2, filter: "grayscale(0%)", cursor: "default" },
+        "-=.4"
+      )
+      .to(
+        `.pet-name-${id}`,
+        {
+          duration: "0.2 !important",
+          backgroundColor: "rgb(253,235,103)",
+          borderRadius: "5px",
+          color: "rgb(253, 135, 103)",
+        },
+        "-=.4"
+      )
+      .to(
+        `.badge-${id}`,
+        {
+          borderColor: "rgb(253,235,103)",
+          borderRadius: "5px",
+          backgroundColor: "rgb(253,235,103)",
+          color: "rgb(40,44,52)",
+        },
+        "<"
+      )
+      .to(`.card-${id}`, { border: "solid 3px rgb(253,235,103)" }, "<")
+      .to(`.${"--" + id}`, {
+        ease: "none",
+        duration: 8,
+        repeat: -1,
+        rotate: 360,
       });
-
-      tl.to(`.${"--" + id}`, { duration: 0.1, translateY: 3 })
-        .to(`.${"--" + id}`, { duration: 0.3, rotateY: 360, translateY: -10 })
-        .to(`.${"--" + id}`, { duration: 0.3, translateY: 0 })
-        .to(
-          `.${"--" + id}`,
-          { duration: 0.2, filter: "grayscale(0%)", cursor: "default" },
-          "-=.4"
-        )
-        .to(
-          `.pet-name-${id}`,
-          {
-            duration: "0.2 !important",
-            backgroundColor: "rgb(253,235,103)",
-            borderRadius: "5px",
-            color: "rgb(253, 135, 103)",
-          },
-          "-=.4"
-        )
-        .to(
-          `.badge-${id}`,
-          {
-            borderColor: "rgb(253,235,103)",
-            borderRadius: "5px",
-            backgroundColor: "rgb(253,235,103)",
-            color: "rgb(40,44,52)",
-          },
-          "<"
-        )
-        .to(`.card-${id}`, { border: "solid 3px rgb(253,235,103)" }, "<")
-        .to(`.${"--" + id}`, {
-          ease: "none",
-          duration: 8,
-          repeat: -1,
-          rotate: 360,
-        });
-    };
+  };
 
   const handleFavorite = () => {
     let updatedFavorites;
