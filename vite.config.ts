6d35973da93d86abbf44ffd2169b2dc8f7d0ca25
demaceo <hdemaceo@gmail.com>
2025-04-29 13:08:45 -0600

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
-  base: "/adoptdontshop",
+  base: "./",
   plugins: [react(), viteCompression()],
   build: {
     minify: "terser",
